0805de23834d04788d5ee4f47e8b457344e3aa2b
ryan <974481066@qq.com>
2021-06-03 07:45:12 -0700

    mysql test

--- dotnet/Controllers/CookieController.cs
+++ dotnet/Controllers/CookieController.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
 
 namespace dotnet
 {
@@ -12,6 +13,32 @@ namespace dotnet
         public string Hello()
         {
             return "Hello";
+        }
+        [HttpGet]
+        [Route("sql")]
+        public string SqlTest()
+        {
+            string result = "";
+            string server = "192.168.1.104,3306";
+            string database = "schema1";
+            string uid = "root";
+            string pwd = "123456";
+            string connstr = $"Server={server};Database={database};Uid={uid};Pwd={pwd};";
+            MySqlConnection mySqlConnection = new MySqlConnection(connstr);
+            try
+            {
+                mySqlConnection.Open();
+                MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+                mySqlCommand.CommandText = "select * from schema1.table1";
+                MySqlDataReader mySqlDataReader = mySqlCommand.ExecuteReader();
+                while(mySqlDataReader.Read()) result += mySqlDataReader["name"];
+                return result;
+            }catch(Exception e)
+            {
+                mySqlConnection.Close();
+                return e.ToString();
+            }
+            
         }
     }
 }
